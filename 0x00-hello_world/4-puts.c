b90fb5b55a26740e4a6717364419a5b6e4136491
Azubike Oluebube <ebubefrank94@gmail.com>
2023-03-16 16:25:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 
 /**
-
  * main - 4-puts.c
-
  * description - Write a C program that prints exactly
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
 
 {
-
-        puts("\"Programming is like building a multilingual puzzle");
-
-
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
