2b7decb625c7010e8b05a8f08d279dd610f271b9
Azubike Oluebube <ebubefrank94@gmail.com>
2023-03-16 16:06:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+
+ * main - 4-puts.c
+
+ * description - Write a C program that prints exactly
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+        puts("\"Programming is like building a multilingual puzzle");
+
+
+        return (0);
+
+}
