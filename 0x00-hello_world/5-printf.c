7b03d4bc7e7e28f7c822ea87af51a88b3219d6f2
Azubike Oluebube <ebubefrank94@gmail.com>
2023-03-16 16:39:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - 5-printf.c
+ * description - write a C program that prints exactly with proper grammer.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
