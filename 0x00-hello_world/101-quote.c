b71c5e38fe14448dfda56e031527d807d8501635
Azubike Oluebube <ebubefrank94@gmail.com>
2023-03-16 17:53:44 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - 101-qoute.c
+ * a c program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	return (1);
+}
