7d75ac9cb41abc78c4012c20079b5d71b5f87ad8
Azubike Oluebube <ebubefrank94@gmail.com>
2023-03-16 18:13:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -8,6 +9,8 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+	char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
